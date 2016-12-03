4e182ce809783409f39fc4f299a4095444ca79a8
nglenno18 <nglenno18@gmail.com>
2016-12-02 19:57:11 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,22 +1,8 @@
-/*
-  Section 7, Lecture 74 --> Testing POST/ todos
 
-  Set up the test suite for the API, writing 2 test cases form /todos
-
-  Verify that when we send the correct data as the body, we get a 200 status code
-
-  Before we do any of this, we have to install new modules
-      -expect for assertions
-      - mocha for test suite
-      - supertest to test express routes
-      - nodemon to create test-watch script so we can auto start the test suite
-
-  //run npm i expect@1.20.2 mocha@3.0.2 nodemon@1.10.2 supertest@2.0.0 --save-dev
-  create new folder server/tests to save test files (test file for server.js)
-
-*/
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose.js');  //CAN leave off the .js ext
 var {Todo} = require('./models/todo');
@@ -44,21 +30,54 @@ app.post('/todos', function(request, response){
   });
 });
 
-
-//SECTION 7, Lecture 75 --> List Resources - GET/todos
-// Creating our SECOND routes
-//   -first screen will probably show a user the list of all todos
 app.get('/todos', function(request, response){
-  //get all todos in collection, which we did in the test file (server.test.js) where we used find
   Todo.find().then(function(todos){
-    //send all the infor back
-    // response.send(todos); //cvould pass back the array like this, but cann add on another custom status code or property
-    //instead, send back an object to open flexibility
     response.send({todos});
   }, function(error){
     response.status(400).send(error);
   });
-});   //IN POSTMAN: start a collection of routes that we will use over again -->save as GET/todos
+});
+
+// Section 7, Lecture 78 --> Getting an Individual Resource - GET /todos/:id
+//
+// GET /todos/123421342423
+//   - fetch that value (make that part dynamic), and USE it to make a QUERY
+app.get('/todos/:id', function(request, response){
+  // //params is a key-value pair
+  // response.send(request.params);
+  var id = request.params.id;
+  //CHALLENGE 78
+  //Validate id using isValid
+    //error is 404 --> send back an empty body
+    if(!ObjectID.isValid(id)){
+      console.log('ID NOT VALID!');
+      console.log('');
+    }
+    //
+  //Query the DB using findById()
+    //success
+      //if todo -- send back
+      //if no todo (call succeed, ID not found in collection) --> 404 empty body
+    //error
+      //400 -
+      Todo.findById(id).then(function(todo){
+        if(!todo){                        //Weeds out VALID, but MISSING id
+          console.log('');
+          response.send({todo});
+          return response.status(404).send('ID MISSING: Valid Id not found!');
+        }
+        console.log('');
+        console.log('User By ID', todo);
+        console.log('');
+        response.send({todo});
+      })
+        .catch(function(error){
+          console.log('ID NOT VALID', id);
+          console.log('        ', error.message);
+          console.log('');
+          response.status(404).send(error);
+        });
+});
 
 app.listen(3000, function(){
   console.log('Started on port 3000');
